7163e260883013b24be01fa7b4d037e0fa862fac
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-09-30 14:56:31 +0300

    MauiCRUDMainPage

--- MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
+++ MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
@@ -1,25 +1,17 @@
-﻿namespace MauiCRUD
+﻿using MauiCRUD.ViewModels;
+
+namespace MauiCRUD
 {
 	public partial class MainPage : ContentPage
 	{
-		int count = 0;
+		private readonly ProductsViewModel _productsViewModel;
 
 		public MainPage()
 		{
 			InitializeComponent();
 		}
 
-		private void OnCounterClicked(object sender, EventArgs e)
-		{
-			count++;
 
-			if (count == 1)
-				CounterBtn.Text = $"Clicked {count} time";
-			else
-				CounterBtn.Text = $"Clicked {count} times";
-
-			SemanticScreenReader.Announce(CounterBtn.Text);
-		}
 	}
 
 }
