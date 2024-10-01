48297819be48fb153e311201f99c1d5a3566deef
DESKTOP-FSFG20J\Kermo <kermoanijarv@gmail.com>
2024-10-01 16:47:17 +0300

    MauiCRUDMainPageFix

--- MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
+++ MauiCRUD/MauiCRUD/MauiCRUD/MainPage.xaml.cs
@@ -6,11 +6,18 @@ namespace MauiCRUD
 	{
 		private readonly ProductsViewModel _productsViewModel;
 
-		public MainPage()
+		public MainPage(ProductsViewModel productsViewModel)
 		{
 			InitializeComponent();
+			BindingContext = productsViewModel;
+			_productsViewModel = productsViewModel;
 		}
 
+		protected async override void OnAppearing()
+		{
+			base.OnAppearing();
+			await _productsViewModel.LoadProductsAsync();
+		}
 
 	}
 
